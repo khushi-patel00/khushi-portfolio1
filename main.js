6ec5bccd03d83893209fdf98ddb192d0a996e8be
khushi-patel00 <133922608+khushi-patel00@users.noreply.github.com>
2023-05-17 19:26:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",
+{
+  strings:["A Public Health Student" , "An Aspiring Health Policy Leader" , "An Aspiring Health Data Analyst"],
+  typeSpeed:100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop:true
+});
